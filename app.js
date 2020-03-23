152f592fd982b9ab583117b6d346f36521cd416f
Devesh Avutu <devesh14may@gmail.com>
2020-03-23 19:28:26 +0530

    added databse support for blog website

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
+const _ = require("lodash");
+const mongoose = require("mongoose");
 const homeStartingContent =
 	"Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutStartingContent =
@@ -18,8 +17,22 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+var blogPostSchema = new mongoose.Schema({
+	title: String,
+	body: String,
+	link: String
+});
+
+var BlogModel = new mongoose.model("Post", blogPostSchema);
+
 app.get("/", function(req, res) {
-	res.render("home", { homeContent: homeStartingContent });
+	const posts = BlogModel.find({}, function(err, posts) {
+		res.render("home", { homeContent: homeStartingContent, blogPosts: posts });
+	});
 });
 
 app.get("/about", function(req, res) {
@@ -34,8 +47,22 @@ app.get("/compose", function(req, res) {
 	res.render("compose");
 });
 
+app.get("/posts/:postTitle", function(req, res) {
+	BlogModel.findOne(
+		{ link: _.lowerCase(req.params.postTitle).replace(/ /g, "-") },
+		function(err, post) {
+			res.render("post", { postTitle: post.title, postBody: post.body });
+		}
+	);
+});
+
 app.post("/", function(req, res) {
-	console.log(req.body.newPost);
+	const newPost = new BlogModel({
+		title: req.body.postTitle,
+		body: req.body.postBody,
+		link: _.lowerCase(req.body.postTitle).replace(/ /g, "-")
+	});
+	newPost.save();
 	res.redirect("/");
 });
 
